2bd49acfe412dcbfbfc98d5ac30b83ca458ea8e0
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-16 15:07:00 +0900

    if~else 기본

---
+++ 04_Control/IfElse_01.java
@@ -0,0 +1,32 @@
+package java04_control.condition;
+
+
+
+public class IfElse_01 {
+
+
+	public static void main(String[] args) {
+		
+		//제어문,else
+//		if(조건식) {
+//			//조건식이 참일 때 수행되는 코드
+//			
+//		}else {
+//			//조건식이 거짓일 때 수행하는 코드
+//		}
+//		
+		//--------------------------------------------------------------------
+		
+		int num=13;
+		
+		if(num==11) { //제어문은 중괄호 없이 코드 한 줄 
+			System.out.println("num은 11이다");
+		} else {
+			System.out.println("num은 11이 아니다");
+		}
+		
+		
+	}
+
+
+}
