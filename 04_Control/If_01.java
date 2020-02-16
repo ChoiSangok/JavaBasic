8bc5d4bc3f1d1f0888210b9bc948403c2be1fa2c
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-16 15:06:18 +0900

    제어문

---
+++ 04_Control/If_01.java
@@ -0,0 +1,26 @@
+package java04_control.condition;
+
+public class If_01 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		//조건문 - if
+		
+//		if(조건식) {
+			//조건식이 참일 경우 수행하는 코드 영역
+
+//		}
+		//조건식은 결과를 true false (boolean)으로 판단
+		
+		if(true) {	//조건식이false 라서 출력이 안된다!
+					//조건식이 true 라서 if문도 출력, if밖은 당연히 출력
+			
+			
+			System.out.println("출력");
+		}
+		System.out.println("if문의 밖");
+		
+	}
+
+}
