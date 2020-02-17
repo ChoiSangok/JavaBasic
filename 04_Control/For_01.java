fb3f9b0af5874cd2ef66d22d0de8afce190fbce9
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-17 20:43:29 +0900

    for 반복문

---
+++ 04_Control/For_01.java
@@ -0,0 +1,37 @@
+package java04_control.loop;
+
+public class For_01 {
+
+	public static void main(String[] args) {
+		
+		//반복문 ,Loop
+		
+		//for / while / do ~ while
+		
+//		for(초기식 ; 조건식 ; 증감식) {
+//			//반복해서 수행할 코드를 작성하는 영역
+//			
+//			//조건식 부분이 참일 때 수행하는 코드
+//			
+//		
+//		}
+		//변수 i가 0부터 5미만동안 i를 +1 씩 증가하면서 코드 수행
+		for (int i=0; i<5;i++) { //i, 0-4, 5번
+			System.out.println(i);
+			
+		}
+		
+		//수행순서
+		//	1. 초기식 수행
+		//	2. 조건식 판별 
+//				2-1 true 일 때 		3. 증감식 수행
+//				2-2 false 일 때		-> for문 종료
+		//	3. 반복코드 1회 수행
+		//	4. 증감식 수행
+		//	5. 2번 수행
+	
+		
+		
+		
+	}
+}
