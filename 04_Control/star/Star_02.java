64f907c999fdf77234d79543d5482b3df5ebe3ea
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-19 17:28:10 +0900

    별찍기2

---
+++ 04_Control/star/Star_02.java
@@ -0,0 +1,17 @@
+package java04_control.star;
+
+public class Star_02 {
+
+	public static void main(String[] args) {
+		 for(int i=0;i<5;i++) {
+	   
+	         System.out.println("*"); //quiz 2번 ***** 세롳
+	         //*
+	         //*
+	         //*
+	         //*
+	         //*
+	      }
+	}
+
+}
