f1c70f164e68e8a7f3cec588d3a2b3f11f0fa223
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-20 19:02:18 +0900

    Create Star_06.java

---
+++ 04_Control/star/Star_06.java
@@ -0,0 +1,22 @@
+package java04_control.star;
+
+public class Star_06 {
+
+	public static void main(String[] args) {
+		
+		int i,j;
+		
+		for (i=1;i<6;i++) { //quiz 6     
+			for(j=i;j<i+5;j++) {        
+				System.out.print(j);      
+			}                       
+			System.out.println();         
+		}
+		//12345
+		//23456
+		//34567
+		//45678
+		//56789
+	}
+
+}
