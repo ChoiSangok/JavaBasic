3030be1afcb2330ed7f9f6af5a464ae8fdd2bf95
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-20 19:02:42 +0900

    별찍기 7

---
+++ 04_Control/star/Star_07.java
@@ -0,0 +1,23 @@
+package java04_control.star;
+
+public class Star_07 {
+	public static void main(String[] args) {
+		
+		int i,j;
+		
+		for (i=5;i>=1;i--) { //quiz 7    
+     
+			for(j=i;j<i+5;j++) {        
+				System.out.print(j);   
+			}                      
+			System.out.println();
+		}
+		//56789
+		//45678
+		//34567
+		//23456
+		//12345
+	}
+
+
+}
