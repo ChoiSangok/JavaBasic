1cc0364fb6c51fcdbe80061c08dd1ff669fd8429
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-21 15:58:52 +0900

    별찍기 9

---
+++ 04_Control/star/Star_09.java
@@ -0,0 +1,26 @@
+package java04_control.star;
+
+public class Star_09 {
+
+
+	public static void main(String[] args) {
+		
+		int i,j;
+		
+		for(i=0; i<5 ; i++) { //quiz 
+      
+			for(j=5;i<j;j--) {
+				System.out.print("*");      
+                              		  
+			}                        	 
+			System.out.println("");        
+		}
+		//*****
+		//****
+		//***
+		//**
+		//*
+   
+	}
+
+}
