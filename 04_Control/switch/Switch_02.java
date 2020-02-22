69d3fdf8001793463d69f4ce95bc7c48e00077f0
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-02-22 23:23:06 +0900

    switch

---
+++ 04_Control/switch/Switch_02.java
@@ -0,0 +1,26 @@
+package java04_control.switchCase;
+
+public class Switch_02 {
+	public static void main(String[] args) {
+		
+		
+		//switch는 비교대상으로 연산식을 넣을 수 있다.
+		
+		int num=20;
+		
+		switch(num-5) {
+		
+		case 10:
+			System.out.println("1번 영역");
+			break;
+		case 15:
+			System.out.println("1번 영역");
+			break;
+		case 20:
+			System.out.println("1번 영역");
+			break;
+			
+		
+		}
+	}
+}
