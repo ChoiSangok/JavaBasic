cb9b3d12f186f9061f65e3bcb780875a7a6a6efb
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-03 11:27:37 +0900

    기능클래스 - 접근제한자private  이용

---
+++ 06_Class/Class_02.java
@@ -0,0 +1,27 @@
+package java06_class;
+
+
+
+//기능 클래스 
+public class Class_02 {
+	
+	//접근제한자를 적용한 멤버 필드
+	
+	private int num1=10;
+	
+	int num2=20;
+	
+	protected int num3=30;
+	
+	public int num4=40;
+	
+	
+	public void method() {
+		System.out.println(num1);
+	}
+	
+	
+	
+
+
+}
