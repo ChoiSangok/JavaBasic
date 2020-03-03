d5e14f4c0f3bdc4afbe1aa903d19dc3a5c475d7c
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-03 11:28:04 +0900

    클래스 private ,getter setter

---
+++ 06_Class/Class_03.java
@@ -0,0 +1,22 @@
+package java06_class;
+
+public class Class_03 {
+	
+	private int num;//번호
+	private String naem;;//이름
+	private double height;//키
+	
+	
+	//Getter Method- num 필드의 값을 알려준다(반환한다)
+	public int getNum() {
+		return num;
+	}
+	
+	
+//	Setter Method -num필드에 값을 저장한다
+	public void setNum(int num) {
+		this.num=num;
+		
+	}
+
+}
