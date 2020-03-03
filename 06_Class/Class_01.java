1c2e69f13c055b4580e353acb0c91710b19c7bda
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-03 11:27:07 +0900

    기능클래스 - 멤버필드 멤버메소드

---
+++ 06_Class/Class_01.java
@@ -0,0 +1,18 @@
+package java06_class;
+
+
+//기능 클래스-main() 메소드 없이 기능만 가진 클래스
+
+public class Class_01 {
+	
+	//멤버 필드 (Member Field) - 데이터, 정보, 속성 
+	int num;
+	String str;
+	
+	//멤버 메소드(Member Method) - 기능, 행위, 동작 function
+	public void display() {
+		System.out.println("메소드");
+	}
+
+
+}
