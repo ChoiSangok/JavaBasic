6b81a4de37fb27871a0c3cf8d93a8a0b35621d88
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-05 01:15:07 +0900

    메소드 배열

---
+++ 06_Class/method/Method_03.java
@@ -0,0 +1,31 @@
+package java06_class.method;
+
+public class Method_03 {
+
+	public void arrTest01(int[] arr) { //int[] 배열, 참조형
+		
+		arr[2]=888;
+		
+		
+	}
+	public void arrTest02(int num) {//기본 데이터타입
+		num=8789;
+		
+	}
+	
+	//--------------------------------------------------------
+	
+	public int arrTest03() {
+		int num=10;
+		
+		return num;
+		
+	}
+	public int[] arrTest04() {
+		int[] arr=new int[3];
+		
+		return arr;
+		
+	}
+
+}
