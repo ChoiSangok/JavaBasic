63939ec21de8e4dc1176d6c00c12bed76d858fdf
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-04 19:05:04 +0900

    getter setter alt+shift+s

---
+++ 06_Class/field/MemberField_01.java
@@ -0,0 +1,30 @@
+package java06_class.field;
+
+public class MemberField_01 { //기능클래스
+
+	//멤버필드
+	//접근제한자 private
+	private int num=888;
+	private String name="Alice";
+	//Generate Getter and Setters 단축키 : alt+ shift+ s
+
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+
+	
+	
+	
+}
