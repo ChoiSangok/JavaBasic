382eb8b2769bee9d675b6271a29d7f477bfa4839
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-08 20:23:34 +0900

    학생정보

---
+++ 06_Class/manage/Student.java
@@ -0,0 +1,60 @@
+package java06_class.manage;
+
+public class Student {
+
+   private String name;
+   private int age;
+   
+   private int kor;
+   private int eng;
+   private int math;
+   
+   private int sum;
+   private double avg;
+   
+   
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public int getAge() {
+      return age;
+   }
+   public void setAge(int age) {
+      this.age = age;
+   }
+   public int getKor() {
+      return kor;
+   }
+   public void setKor(int kor) {
+      this.kor = kor;
+   }
+   public int getEng() {
+      return eng;
+   }
+   public void setEng(int eng) {
+      this.eng = eng;
+   }
+   public int getMath() {
+      return math;
+   }
+   public void setMath(int math) {
+      this.math = math;
+   }
+   public int getSum() {
+      return sum;
+   }
+   public void setSum(int sum) {
+      this.sum = sum;
+   }
+   public double getAvg() {
+      return avg;
+   }
+   public void setAvg(double avg) {
+      this.avg = avg;
+   }
+   
+   
+}
