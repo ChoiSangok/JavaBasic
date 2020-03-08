9ec96f5107860a5400cd0eaed8515932e9404ccb
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-08 20:25:21 +0900

    Create StudentService_ver2.java

---
+++ 06_Class/manage2/StudentService_ver2.java
@@ -0,0 +1,73 @@
+//package java06_class.manage2;
+//
+//import java.util.Scanner;
+//
+//import java06_class.manage.Student;
+//
+//public class StudentService_ver2 {
+//
+//	//sc :scanner
+//	//stuArr[]=Student;
+//	Student stu=new Student();
+//	Scanner input=new Scanner(System.in);
+//
+//	Student[] stuArr=new Student[3]; //참조형 변수 학생수 3명
+//	
+//	
+//
+//	//생성자
+//	public StudentService_ver2() {
+//		
+//	}
+//	
+//	public void getIdx() {
+//		
+//	}
+//	public void insertinfo(Student stuArr,int) { //학생이름ㅇ, 123
+//		
+//
+//	}
+//	public void insertInfoldx() {
+//		
+//	}
+//	public void insertScore(Student stuArr,int) {//학생이름에 점수
+//		
+//	}
+//	public void insertScoreAll() {
+//		
+//	}
+//	public void insertScoreIdx() {
+//	
+//	}
+//
+//	public void insetStudAll() {
+//		
+//		
+//	}
+//	public void calcSum(Student) { //계산
+//		
+//	}
+//	public void clacAvg(Student) { //평균
+//		
+//	}
+//	public void printStud(Student,int) { //학생이름ㅇ, 점ㅅ
+//		
+//	}
+//	public void printStuAll() {
+//		
+//	}
+//	public void printStuIdx() {
+//		
+//	}
+//	
+//	
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
