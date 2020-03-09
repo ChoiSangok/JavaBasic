90002301c698695ab4cc4077d6a803129de8ebe2
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-09 16:14:58 +0900

    자바 기본 구성요소

---
+++ 01_Variables/VariablesEx_01.java
@@ -0,0 +1,19 @@
+//소스코드가 포함된 패키지를 나타낸다
+package java01_variables;
+
+//클래스 정의(자바 프로그램의 기본 구성요소, 객체지항 파트에서 자세히 다룸)
+public class VariablesEx_01 {
+		//메인 메소드 정의 - 프로그램의 시작점(Entry Point, EP)
+		public static void main(String[] args) {
+		
+			System.out.println("하이!"); // 모니터에 "하이!"를 출력하는 코드
+			
+			int number;
+			number = 11; //변수에 값 대입하기 - 변수 공간에 데이터 저장하기
+			
+			System.out.println(number); //모니터에 변수에 저장된 값 출력한다
+		}// main end - 프로그램 종료점
+		
+} // End class
+
+
