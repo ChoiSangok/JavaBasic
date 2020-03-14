88ac8d8fa381dcdb7a482048b46957a6dd370147
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-14 17:28:49 +0900

    실행클래스

---
+++ 08_abstract/interfaceEx/quiz/TaxiEx.java
@@ -0,0 +1,17 @@
+package java08_abstract.interfaceEx.quiz;
+
+public class TaxiEx {
+
+	public static void main(String[] args) {
+		
+		Taxi taxi=new Taxi();
+		
+		//요금
+		taxi.calc(2500);
+		
+		//출력
+		//calc() 메소드를 이용하여 출력한다
+		
+		//2500m 운행했을때 요금은 xxxx원입니다
+	}
+}
