12ef30f0e625f4aa344e4af84bce7fa3ecdb4ac1
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-03-20 15:29:08 +0900

    북실행클래스

---
+++ 10_collection/practice2/BookManageMapEx.java
@@ -0,0 +1,46 @@
+package java10_collection.practice2;
+
+import java.util.Scanner;
+
+public class BookManageMapEx {
+
+   public static void main(String[] args) {
+//      1. 새 도서 추가   //putBook(inputBook() 이 리턴한 객체) 실행
+//      2. 도서 삭제   //removeBook(삭제할 책번호) 실행
+//      3. 도서 검색출력   //searchBook( 검색할 도서명) 실행 
+//            => index 리턴받아 printBook(key) 실행
+//      4. 전체 출력   //displayAll() 실행
+//      6. 끝내기
+      
+      Scanner sc= new Scanner(System.in);
+      
+   
+      BookManageMap bmm=new BookManageMap();
+
+
+
+   }
+   
+   public static Book inputBook() {
+      
+      
+      System.out.println("도서추가");
+      
+      return null;
+      
+      
+   }
+   
+
+   public static String inputBookTitle() {
+      
+      
+      
+      return null;
+      
+      
+   }
+   
+
+
+}
