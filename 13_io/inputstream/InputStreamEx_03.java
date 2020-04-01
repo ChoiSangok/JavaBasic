3e14efde99919e319093b61998c55a9d2445e083
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-04-01 16:38:33 +0900

    InputStream, StringBuilder

---
+++ 13_io/inputstream/InputStreamEx_03.java
@@ -0,0 +1,77 @@
+package java13_io.InputStream;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputStreamEx_03 {
+
+   public static void main(String[] args) {
+      
+      
+      InputStream is= System.in; //표준 입력 객체
+      
+      byte[] b= new byte[1024]; //입력데이터의 임시 저장소
+      int len=-1;// 입력받은 데이터의 길이
+      int total=0; //입력한 데이터의 전체 길이
+      
+      
+      
+      StringBuilder sb= new StringBuilder(); //입력받은 데이터 저장소
+      
+      System.out.println("<< 입력 대기중 >>");
+      
+      
+      try {
+         while( (len = is.read(b)) != -1) {
+            
+            total += len; //전체 입력 길이 계산
+            
+            System.out.println("len : "+ len); //입력받은 길이
+            
+            String str= new String(b,0,len);
+            System.out.println("str : "+str);
+//            System.out.println("b : " + b);
+            
+            
+            //new String(b) 로 변환하면 byte[] 에 모든 요소를 변환한다
+            // 이 때 이전에 입력했던 데이터도 같이 변환해버린다
+            // new String(b,0,len) 을 이용하여 입력한 길이만큼만
+            // 변환하여 사용한다
+            
+         }
+         
+         
+         
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      System.out.println(" 전체  : "+ total+" Byte입력완료");
+      System.out.println("\n<< 입력 데이터 >>");
+      System.out.println(sb);
+      
+      
+      
+   }
+
+}
+
+
+
+//
+//
+// - public int read(byte[] b) throws IOException
+//       입력스트림에서 최대 b 배열의 길이만큼 읽어들인다
+//       
+//       읽은 데이터 b배열의 0번째 인덱스부터 기록한다
+//       
+//       읽어들인 데이터의 길이를 int형으로 반환한다
+//       
+//       읽어들일 데이터가 없을 경우(EOF) -1을 반환한다
+//       
+//       
+
+
+
+
